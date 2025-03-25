be9b6ad9748746f364e81b22b59e2d2c64e258b0
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-03-25 04:55:22 +0000

    feat: add react-helmet for improved SEO and page metadata management in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,18 @@ import Locations from './home/Locations'
 import References from './home/References'
 import Links from './home/Links'
 import Gallery from './home/Gallery'
+import {Helmet} from "react-helmet"
 
 function Home() {
     return (
         <div className="">
+            <Helmet>
+                <title>Study Abroad, Overseas Education Consultants, Get Counselling! | MEC India</title>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width"/>
+                <meta name="description" content="Study Abroad Consultants - MEC education is an Overseas Consultancy, we help students to study overseas in Australia, Germany, Canada, UK &amp; New Zealand."/>
+                <link rel="canonical" href="http://mec.in" />
+            </Helmet>
             <Carousel />
             <About />
             <Locations />
