7021d34ea06198211ed51c8344261145574dd8fc
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-03-30 15:02:39 +0000

    feat: move web app manifest to public directory and update reference in index.html

--- vite.config.js
+++ vite.config.js
@@ -9,42 +9,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      manifest: {
-        name: "Mayilon Educational Consultant",
-        short_name: "MEC",
-        description:
-          "Study Abroad Consultants - MEC education is an Overseas Consultancy, we help students to study overseas in Australia, Germany, Canada, UK & New Zealand.",
-        theme_color: "#800000",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/pwa-maskable-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "/pwa-maskable-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ]
-      },
       registerType: "autoUpdate",
       workbox: {
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
