e943a606319c87017e966439f4d0afaa0991da5b
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-04-03 05:56:41 +0000

    feat: add Google Analytics integration with react-ga4 for pageview tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-8G2SQMQMH2");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
