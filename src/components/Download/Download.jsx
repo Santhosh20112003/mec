ab45014a1578ec48b0efc210ed4aa36d41be1e97
abinandhan2003 <abinandhan77@gmail.com>
2025-05-23 16:00:13 +0530

    Update download link in Download component to point to Google Drive

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -7,7 +7,7 @@ function Download() {
     <div className="text-center py-10">
       <h1 className="text-2xl font-bold mb-4">Download our Flyer</h1>
        <a
-            href="/1.pdf"
+            href="https://drive.google.com/uc?export=download&id=1tZfqqkHcNAEbFnGp7-c6-x5-sCs632LP"
             download
             className="font-poppins text-sm font-medium px-4 py-2 bg-accent shadow-md rounded-md bg-[#800000] text-white transition-colors"
             onClick={() => alert('The resume has been downloaded!')}
