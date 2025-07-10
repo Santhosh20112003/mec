f96bc770ea1c253b15704bdde11399930fd32f72
Mayilon Educational Consultant <mayiloneducation@gmail.com>
2025-07-10 11:21:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-8G2SQMQMH2");
 ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-
+// update
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
